cf680f7852fecbe2589b84abe6692f5633d217bc
venkatesh <venkateshyc353@gmail.com>
2019-03-23 13:10:15 +0530

    firstcommit

---
+++ firstproject1/college/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import  render
+from . forms import schoolform
+def school_info(request):
+    if request.method=='POST':
+        infoo=schoolform(request.POST)
+        print(10/0)
+        if infoo.is_valid():
+            print(infoo.cleaned_data['name'])
+            print(infoo.cleaned_data['number'])
+            print(infoo.cleaned_data['salary'])
+    go=schoolform()
+    return  render(request,'testapp/register.html',{'go':go})
